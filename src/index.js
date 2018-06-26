d3df04c0821f80e7875cec88f533238867f6910c
slavco zute <slav.zute@jdplc.com>
2018-06-26 10:12:57 +0100

    get JSON and render data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
+  let products = []
   $.getJSON('data.json', function(data) {
-    $('#jsonData').html(JSON.stringify(data));
+    data.products.forEach(product => {
+      products.push(product)
+    });
+    console.info(products);
+    return products
+  }).then(products => {
+    console.info(products.products)
+    products.products.forEach(product => {
+      $('#jsonData').append(`<li>${product.title}</li>`)
+    });
   })
 })
