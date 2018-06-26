f1ef38be939e769aac69dc12da3f434aa5f08636
slavco zute <slav.zute@jdplc.com>
2018-06-26 23:56:39 +0100

    get all available prices in products

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-  const productCol = []
+  const products = []
   const table = $('#jsonData')
+  const prices = []
+  const colors = []
+  const categories = []
 
   function getVariants(variants) {
     console.info(variants)
@@ -8,13 +11,30 @@ $(document).ready(function() {
     }
   }
 
+  function getCollection(value, array) {
+    if ($.inArray(value, array) === -1) {
+      array.push(value)
+    }
+  }
+
   $.getJSON('data.json', function(data) {
     data.products.forEach(product => {
-      productCol.push(product)
+      products.push(product)
+      // console.info(product.variants.length)
+      if (product.variants.length > 1) {
+        product.variants.forEach(variant => {
+          getCollection(variant.price, prices)
+          // console.info(prices);
+        })
+      } else {
+        getCollection(product.variants[0].price, prices);
+      }
+
     });
   })
   .then(() => {
-    productCol.forEach(product => {
+    console.info(prices);
+    products.forEach(product => {
       table.append(`
       <tr>
         <td>${product.title}</td>
