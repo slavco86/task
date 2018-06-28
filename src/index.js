fa65403c1db2100b1fca139d953c35bb829f26ea
slavco zute <slav.zute@jdplc.com>
2018-06-28 03:52:22 +0100

    refactor code

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ $(document).ready(function() {
   const colors = []
   const categories = []
 
-  function getVariants(variants) {
-    console.info(variants)
-    if (variants.length > 0) {
-    }
-  }
-
   function getCollection(value, array) {
     if ($.inArray(value, array) === -1) {
       array.push(value)
@@ -20,20 +14,13 @@ $(document).ready(function() {
   $.getJSON('data.json', function(data) {
     data.products.forEach(product => {
       products.push(product)
-      // console.info(product.variants.length)
-      if (product.variants.length > 1) {
-        product.variants.forEach(variant => {
-          getCollection(variant.price, prices)
-          // console.info(prices);
-        })
-      } else {
-        getCollection(product.variants[0].price, prices);
-      }
-
+      product.variants.forEach(variant => {
+        getCollection(variant.price, prices)
+      })
     });
   })
   .then(() => {
-    console.info(prices);
+    console.info('Prices: ' + prices);
     products.forEach(product => {
       table.append(`
       <tr>
